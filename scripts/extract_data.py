9d7a5d5b3e33575eed118dd59cc932369a774ea5
dane1k <danekdb.nz@gmail.com>
2025-05-29 13:31:45 +1200

    cleaned project structure, ignore secrets and data

---
+++ scripts/extract_data.py
@@ -0,0 +1,28 @@
+import pandas as pd
+import requests
+import os
+
+API_KEY = os.getenv("TMDB_API_KEY")
+url = 'https://api.themoviedb.org/3/discover/movie'
+params = {
+    'api_key': API_KEY,
+    'primary_release_date.gte': '2010-01-01',
+    'primary_release_date.lte': '2020-12-31',
+    'sort_by': 'popularity.desc',
+    'page': 1
+}
+
+response = requests.get(url, params=params)
+data = response.json()
+for movie in data['results']:
+    print(movie['title'], movie['release_date'], movie['vote_average'])
+
+all_movies = []
+for page in range(1, 11):
+    params['page'] = page
+    response = requests.get(url, params=params)
+    data = response.json()
+    all_movies.extend(data['results'])
+
+df = pd.DataFrame(all_movies)
+df.to_csv('tmdb_movies_2010_2020.csv', index=False)
